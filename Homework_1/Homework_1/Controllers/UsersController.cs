33d1956b97d8ddbe34e36770c1db7f76218b8185
Elena-r-code <rendevskaelena@gmail.com>
2021-09-11 20:24:44 +0200

    Homework

---
+++ Homework_1/Homework_1/Controllers/UsersController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> GetAllUsers()
+        {
+            try
+            {
+                return StatusCode(StatusCodes.Status200OK, StaticDb.Users);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error has occured");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<string> GetUser(int id)
+        {
+            try
+            {
+                if(id < 0)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, "You have entered a negative number");
+                }
+                else if(id >= StaticDb.Users.Count)
+                {
+                    return StatusCode(StatusCodes.Status400BadRequest, $"The id you have entered is invalid");
+                }
+                return StatusCode(StatusCodes.Status200OK, StaticDb.Users[id]);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error has occured");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult AddUser()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(Request.Body))
+                {
+                    string user = sr.ReadToEnd();
+                    StaticDb.Users.Add(user);
+                    return StatusCode(StatusCodes.Status201Created, "You added a new user");
+                }
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error has occured");
+            }
+        }
+        [HttpDelete]
+        public IActionResult DeleteUser()
+        {
+            try
+            {
+                using(StreamReader sr = new StreamReader(Request.Body))
+                {
+                    string requestBody = sr.ReadToEnd();
+                    int id = int.Parse(requestBody);
+                    if(id < 0)
+                    {
+                        return StatusCode(StatusCodes.Status400BadRequest, "You entered a negative number");
+                    }
+                    else if(id >= StaticDb.Users.Count)
+                    {
+                        return StatusCode(StatusCodes.Status400BadRequest, $"The id you entered is invalid!");
+                    }
+                    StaticDb.Users.RemoveAt(id);
+                    return StatusCode(StatusCodes.Status204NoContent, $"You deleted the User with ID {id}");
+                }
+            }
+            catch(Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Server error has occured");
+            }
+        }
+    }
+}
---
+++ Homework_1/Homework_1/StaticDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Homework_1
+{
+    public static class StaticDb
+    {
+        public static List<string> Users = new List<string>
+        {
+            "Elena Betinska",
+            "Aleksandar Ivanovski",
+            "Mila Velkovksa",
+            "Viktor Stojkov",
+            "Andrej Ivanov",
+            "Danilo Borozan"
+        };
+    }
+}
